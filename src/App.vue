43935e84cb8cc51f3871d636411b3d4cc5c0c3c2
henri-kapidani <h.kapidani@gmail.com>
2023-05-17 15:12:30 +0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+	import TitleComponent from "./components/TitleComponent.vue";
+
+	export default {
+		components: {
+			TitleComponent,
+		},
+	};
+</script>
+
+<template>
+	<TitleComponent />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/TitleComponent.vue
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<template>
+	<h1>Ciao a tutti</h1>
+	<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate similique nobis saepe a earum facere, eius quibusdam, ratione distinctio, ipsam quisquam odio culpa cupiditate. Explicabo qui veniam tenetur corrupti eum!</p>
+</template>
+
+<style scoped>
+
+</style>
